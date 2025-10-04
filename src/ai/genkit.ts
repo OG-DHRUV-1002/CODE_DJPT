cf899878def23679217046fcaf01831dcd0dae71
Dhruv Hadal <dhruvhadal@gmail.com>
2025-10-05 00:25:41 +0530

    Update genkit.ts

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,14 +1,7 @@
-// src/ai/genkit.ts
-
-import { genkit } from 'genkit';
-import { googleAI } from '@genkit-ai/googleai';
+import {genkit} from 'genkit';
+import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [
-    googleAI({
-      // Add this line to pass your API key
-      apiKey: process.env.AIzaSyCRg3EdmVW2iGp12M37RsgZMqxMkSMhs2c, 
-    }),
-  ],
+  plugins: [googleAI()],
   model: 'googleai/gemini-2.0-flash',
 });
